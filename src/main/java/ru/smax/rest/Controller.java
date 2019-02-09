25a86855938a0888bce7704c200bef07e4c9d9b2
Max Samoylov <feardude@gmail.com>
2019-02-09 22:23:12 +0300

    Rest service basic implementation

---
+++ src/main/java/ru/smax/rest/Controller.java
@@ -0,0 +1,17 @@
+package ru.smax.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.format;
+
+@RestController
+@RequestMapping(path = "/api")
+public class Controller {
+    @GetMapping("/hello")
+    public String test(@RequestParam String name) {
+        return format("Hello, %s!", name);
+    }
+}
